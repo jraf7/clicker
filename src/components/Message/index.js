bdb3c1fde826c58486da9a490b414c0d3be7f1a1
Jonathan Johnson <jraf7@icloud.com>
2020-01-18 19:07:13 -0500

    add message

---
+++ src/components/Message/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Message (props) {
+    return (
+    <h3>{props.state.message}</h3>
+    )
+}
+
+export default Message;
--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Character from './Character/index';
 import Footer from './Footer/index';
 import Header from './Header/index';
 import Wrapper from './Wrapper/index';
+import Message from './Message/index';
 
 export {
     Character, 
+    Message,
     Footer, 
     Header,
     Wrapper
